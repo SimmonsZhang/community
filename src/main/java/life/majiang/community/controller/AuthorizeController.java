04ca772b8782b2f69232653df0b0a268b7d229bf
zhangsimin <1634703459@qq.com>
2020-03-18 17:15:44 +0800

    getUser

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,45 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    String clientId;
+    @Value("${github.client.secret}")
+    String clientSecret;
+    @Value("${github.redirect.uri}")
+    String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code, @RequestParam("state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+
+        try {
+            GithubUser user = githubProvider.getUser(accessToken);
+            System.out.println(user.getLogin());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "index";
+    }
+}
